eccaf7dbbc81e1a11d0b68549957868921aa085f
df-1945 <daffaalfarizi1945@gmail.com>
2022-10-07 10:35:37 +0700

    tes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<!-- <html>
   <head>
     <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
   </head>
@@ -10,4 +10,5 @@
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
   </body>
-</html>
+</html> -->
+<?php echo "This message is from server side." ?>
